980fd1139bbf9c860af40af0a30d8a5f472a1ffd
Festus Ebine <YOUR_GITHUB_EMAIL>
2026-08-06 10:10:41 +0100

    Sprint 4A: Wire payment and licensing repositories

--- services/api/src/licensing/licensing.module.ts
+++ services/api/src/licensing/licensing.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { License } from './entities/license.entity';
 import { LicensingService } from './licensing.service';
 import { LicensingController } from './licensing.controller';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([License]),
+  ],
   providers: [LicensingService],
-  controllers: [LicensingController]
+  controllers: [LicensingController],
+  exports: [LicensingService],
 })
 export class LicensingModule {}
