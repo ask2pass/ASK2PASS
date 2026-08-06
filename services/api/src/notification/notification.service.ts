a1e48add64923c232a2d23d4b4780171f66c2b14
Festus Ebine <YOUR_GITHUB_EMAIL>
2026-08-06 05:34:07 +0100

    Sprint 3E: Implement notification lifecycle service foundation

--- services/api/src/notification/notification.service.ts
+++ services/api/src/notification/notification.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { NotificationLog } from './entities/notification-log.entity';
+import { User } from '../user/entities/user.entity';
 
 @Injectable()
-export class NotificationService {}
+export class NotificationService {
+  constructor(
+    @InjectRepository(NotificationLog)
+    private readonly notificationRepository: Repository<NotificationLog>,
+  ) {}
+
+  async createNotification(
+    user: User,
+    type: string,
+  ): Promise<NotificationLog> {
+    const notification = this.notificationRepository.create({
+      user,
+      type,
+      delivered: false,
+    });
+
+    return this.notificationRepository.save(notification);
+  }
+
+  async markDelivered(
+    id: string,
+  ): Promise<NotificationLog> {
+    const notification =
+      await this.notificationRepository.findOneBy({ id });
+
+    if (!notification) {
+      throw new Error('Notification not found');
+    }
+
+    notification.delivered = true;
+    notification.deliveredAt = new Date();
+
+    return this.notificationRepository.save(notification);
+  }
+}
