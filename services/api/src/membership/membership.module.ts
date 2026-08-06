725f83d434c90218052c618e123cea9be9b0e20f
Festus Ebine <YOUR_GITHUB_EMAIL>
2026-08-06 04:50:02 +0100

    Sprint 3C: Wire Membership entity repository

--- services/api/src/membership/membership.module.ts
+++ services/api/src/membership/membership.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Membership } from './entities/membership/membership';
 import { MembershipService } from './membership.service';
 import { MembershipController } from './membership.controller';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Membership]),
+  ],
   providers: [MembershipService],
-  controllers: [MembershipController]
+  controllers: [MembershipController],
+  exports: [MembershipService],
 })
 export class MembershipModule {}
