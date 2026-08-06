b7638524b1962760da8f7f7ae46c3da09490be38
Festus Ebine <YOUR_GITHUB_EMAIL>
2026-08-06 05:04:47 +0100

    Sprint 3D: Implement subscription activation engine foundation

--- services/api/src/subscription/subscription.service.ts
+++ services/api/src/subscription/subscription.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { Subscription } from './entities/subscription.entity';
 import { SubscriptionPlan } from './entities/subscription-plan.entity';
+import { User } from '../user/entities/user.entity';
 
 @Injectable()
 export class SubscriptionService {
@@ -15,11 +16,33 @@ export class SubscriptionService {
     private readonly subscriptionPlanRepository: Repository<SubscriptionPlan>,
   ) {}
 
-  getSubscriptionRepository(): Repository<Subscription> {
-    return this.subscriptionRepository;
-  }
+  async activateSubscription(
+    user: User,
+    planId: string,
+  ): Promise<Subscription> {
+    const plan = await this.subscriptionPlanRepository.findOne({
+      where: { id: planId },
+    });
 
-  getSubscriptionPlanRepository(): Repository<SubscriptionPlan> {
-    return this.subscriptionPlanRepository;
+    if (!plan) {
+      throw new NotFoundException('Subscription plan not found');
+    }
+
+    const start = new Date();
+    const end = new Date(
+      start.getTime() + plan.durationDays * 24 * 60 * 60 * 1000,
+    );
+
+    const subscription = this.subscriptionRepository.create({
+      user,
+      plan,
+      startDate: start,
+      endDate: end,
+      accessExpiresAt: end,
+      active: true,
+      renewalCount: 0,
+    });
+
+    return this.subscriptionRepository.save(subscription);
   }
 }
