7c2d74c1776e7decd7d402a0f78778938f2b1afd
Festus Ebine <YOUR_GITHUB_EMAIL>
2026-08-06 16:12:16 +0100

    Sprint 5A: Wire learner profile repository

--- services/api/src/learner-profile/learner-profile.module.ts
+++ services/api/src/learner-profile/learner-profile.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
-import { LearnerProfileService } from './learner-profile.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { LearnerProfile } from './entities/learner-profile.entity';
 import { LearnerProfileController } from './learner-profile.controller';
+import { LearnerProfileService } from './learner-profile.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      LearnerProfile,
+    ]),
+  ],
+  controllers: [LearnerProfileController],
   providers: [LearnerProfileService],
-  controllers: [LearnerProfileController]
+  exports: [LearnerProfileService],
 })
 export class LearnerProfileModule {}
