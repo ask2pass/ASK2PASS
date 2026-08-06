c15c950993f1a1bad2cc264853f9a47a4fcc92ed
Festus Ebine <YOUR_GITHUB_EMAIL>
2026-08-06 05:32:56 +0100

    Sprint 3E: Wire notification log repository

--- services/api/src/notification/notification.module.ts
+++ services/api/src/notification/notification.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { NotificationLog } from './entities/notification-log.entity';
 import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([NotificationLog]),
+  ],
   providers: [NotificationService],
-  controllers: [NotificationController]
+  controllers: [NotificationController],
+  exports: [NotificationService],
 })
 export class NotificationModule {}
