10fced18235e3422c4d51fb04099135855be8ede
Festus Ebine <YOUR_GITHUB_EMAIL>
2026-08-06 11:00:41 +0100

    Sprint 4D: Add wallet and transaction entities

---
+++ services/api/src/wallet/entities/wallet-transaction.entity.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, ManyToOne } from 'typeorm';
+import { BaseEntity } from '../../common/entities/base.entity';
+import { Wallet } from './wallet.entity';
+import { WalletTransactionType } from '../enums/wallet-transaction-type.enum';
+
+@Entity('wallet_transactions')
+export class WalletTransaction extends BaseEntity {
+  @ManyToOne(() => Wallet, { nullable: false })
+  wallet!: Wallet;
+
+  @Column({
+    type: 'enum',
+    enum: WalletTransactionType,
+  })
+  type!: WalletTransactionType;
+
+  @Column({
+    type: 'integer',
+  })
+  coins!: number;
+
+  @Column()
+  description!: string;
+}
