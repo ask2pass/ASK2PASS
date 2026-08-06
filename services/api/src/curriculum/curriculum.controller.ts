6504dac858c4b0a46cc15b63147d6a01fabb85cb
Festus Ebine <YOUR_GITHUB_EMAIL>
2026-08-06 16:47:11 +0100

    Sprint 5B: Add curriculum controller foundation

--- services/api/src/curriculum/curriculum.controller.ts
+++ services/api/src/curriculum/curriculum.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('curriculum')
-export class CurriculumController {}
+export class CurriculumController {
+  @Post()
+  async createCurriculum() {
+    return {
+      message: 'Curriculum creation endpoint scaffolded',
+    };
+  }
+
+  @Get(':programme')
+  async getProgramme(
+    @Param('programme') programme: string,
+  ) {
+    return {
+      programme,
+      categories: [
+        'PRIMARY',
+        'JUNIOR_SECONDARY',
+        'SENIOR_SECONDARY',
+        'CERTIFICATE_EXAMINATION',
+        'ADULT_EDUCATION',
+        'SAP',
+        'BUSINESS_MODELLING',
+      ],
+      certificateExaminations: [
+        'BECE',
+        'WASSCE',
+        'NECO',
+        'NABTEB',
+        'WAEC_GCE',
+        'NECO_GCE',
+        'A_LEVEL',
+        'JAMB',
+        'POST_UTME',
+      ],
+    };
+  }
+}
