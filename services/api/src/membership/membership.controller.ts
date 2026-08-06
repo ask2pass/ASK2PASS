a22c1e16b03d640899420f065c764401adde567a
Festus Ebine <YOUR_GITHUB_EMAIL>
2026-08-06 04:53:29 +0100

    Sprint 3C: Add Membership access endpoint

--- services/api/src/membership/membership.controller.ts
+++ services/api/src/membership/membership.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { MembershipService } from './membership.service';
 
 @Controller('membership')
-export class MembershipController {}
+export class MembershipController {
+  constructor(
+    private readonly membershipService: MembershipService,
+  ) {}
+
+  @Get('access/:userId')
+  async getAccess(@Param('userId') userId: string) {
+    return this.membershipService.getAccessStatus(userId);
+  }
+}
