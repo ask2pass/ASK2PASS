294dd9172bd63bed6128e38cd7839eb090246824
Festus Ebine <YOUR_GITHUB_EMAIL>
2026-08-06 11:01:54 +0100

    Sprint 4D: Wire wallet repositories

--- services/api/src/wallet/wallet.module.ts
+++ services/api/src/wallet/wallet.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { Wallet } from './entities/wallet.entity';
+import { WalletTransaction } from './entities/wallet-transaction.entity';
 import { WalletController } from './wallet.controller';
 import { WalletService } from './wallet.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Wallet,
+      WalletTransaction,
+    ]),
+  ],
   controllers: [WalletController],
   providers: [WalletService],
   exports: [WalletService],
